b74077973d9916bfbdbb1cdc7e4845640fdd42d8
Ken Ojiri <ozzozz@gmail.com>
2022-11-24 17:30:44 +0900

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World! Hello Tanzu!\n');
+  res.send('Hello World! Hello Tanzu! Hello HiSoL!\n');
 });
 
 app.listen(PORT, HOST);
